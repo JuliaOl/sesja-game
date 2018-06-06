b2d33e461c1a63b37fe9961fc64d84145b28fe0d
Julia Wojtaś <julia.mwojtas@gmail.com>
2018-06-06 15:31:25 +0200

    Integration with a phone and VR glasses

--- Game2/Assets/Scripts/RaycastShoot.cs
+++ Game2/Assets/Scripts/RaycastShoot.cs
@@ -33,6 +33,7 @@ public class RaycastShoot : MonoBehaviour
     void Update()
     {
         // Check if the player has pressed the fire button and if enough time has elapsed since they last fired
+
         if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
             // Update the time when our player can fire next
--- Game2/Assets/Scripts/camraRotation.cs
+++ Game2/Assets/Scripts/camraRotation.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.XR;
+
 
 public class camraRotation : MonoBehaviour {
 
-    public float speedH = 2.0f;
+    /*public float speedH = 2.0f;
     public float speedV = 2.0f;
 
     private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    private float pitch = 0.0f;*/
 
-
+    GameObject[] eyes = new GameObject[2];
+    string[] eyeAnchorNames = { "LeftEyeAnchor", "RightEyeAnchor" };
 
     // Use this for initialization
     void Start()
@@ -22,10 +23,36 @@ public class camraRotation : MonoBehaviour {
     void Update()
     {
 
-        yaw += speedH * Input.GetAxis("Mouse X");
+        /*yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);*/
+        for (int i = 0; i < 2; ++i)
+        {
+            // If the eye anchor is no longer a child of us, don't use it
+            if (eyes[i] != null && eyes[i].transform.parent != transform)
+            {
+                eyes[i] = null;
+            }
+
+            // If we don't have an eye anchor, try to find one or create one
+            if (eyes[i] == null)
+            {
+                Transform t = transform.Find(eyeAnchorNames[i]);
+                if (t)
+                    eyes[i] = t.gameObject;
+
+                if (eyes[i] == null)
+                {
+                    eyes[i] = new GameObject(eyeAnchorNames[i]);
+                    eyes[i].transform.parent = gameObject.transform;
+                }
+            }
+
+            // Update the eye transform
+            eyes[i].transform.localPosition = InputTracking.GetLocalPosition((XRNode)i);
+            eyes[i].transform.localRotation = InputTracking.GetLocalRotation((XRNode)i);
+        }
 
     }
 }
